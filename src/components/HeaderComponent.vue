f43c42e4d24c87cda559f7cd954d8a72e6c3cbfe
RomeuCajamba <romeucajamba@gmail.com>
2024-03-15 11:50:24 +0100

    feat:Instalação e configuração do TailwindCSS no projeto VueJs

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div>
+        <img alt="Vue logo" class="logo" src="@/assets/smartLogo.webp" width="125" height="125" />
+        <h1>SWShop</h1>
+    </div>
+    
+
+    <div class="wrapper">
+      
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
