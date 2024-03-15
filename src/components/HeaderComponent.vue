34799a0b8a0e92cd908be66dfd777884b103fe7d
RomeuCajamba <romeucajamba@gmail.com>
2024-03-15 14:24:29 +0100

    feat:Instalação e implementação de icones

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,18 +1,37 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import MenuIcon from 'vue-material-design-icons/Menu.vue';
+import MdIconCart from 'vue-material-design-icons/Cart.vue';
+import MdIconMagnify from 'vue-material-design-icons/Magnify.vue';
+
+components: {
+  MenuIcon;
+  MdIconCart;
+  MdIconMagnify;
+};
+
+
 </script>
 
 <template>
-  <header>
-    <div>
-        <img alt="Vue logo" class="logo" src="@/assets/smartLogo.webp" width="125" height="125" />
+  <header class="bg-blue-700 w-screen h-20 flex flex-row space-x-[10rem]">
+
+    <div class="bg-[#fff8CE] flex flex-row w-[3rem] h-[3rem]">
+        <img alt="Vue logo" class="logo" src="@/components/icons/logoSmart.png" width="125" height="125" />
         <h1>SWShop</h1>
     </div>
     
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+      <nav class="bg-red-700">
+        <RouterLink to="/" class="text-[#000]">Home</RouterLink>
+        <RouterLink to="/about" class="text-[#fff]">About</RouterLink>
       </nav>
+
+      <div>
+         <MenuIcon/>
+         <MdIconCart/>
+         <MdIconMagnify/>
+        
+      </div>
   </header>
 
   <RouterView />
